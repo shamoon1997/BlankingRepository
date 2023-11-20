3d0947589b92fb6d6c1b73a2a1b199f4587b65c0
waleedbinalim <waleedbinalim@gmail.com>
2023-11-20 19:24:48 +0500

    chore: props, constants, good progress

--- src/components/common/select/select-dropdown.tsx
+++ src/components/common/select/select-dropdown.tsx
@@ -1,28 +1,34 @@
-import { ChevronIcon, FilterIcon } from "@/assets";
+import { ChevronIcon } from "@/assets";
 import * as Select from "@radix-ui/react-select";
-import React from "react";
+import React, { ReactNode } from "react";
 import SelectItem from "./select-item";
 
 type Props = {
   placeholder?: string;
+  dropdownIcon?: ReactNode;
+  options: {
+    value: string;
+    text: string | JSX.Element;
+    icon?: JSX.Element;
+  }[];
 };
 
 const SelectDropdown: React.FC<Props> = ({
   placeholder = "placeholder...",
+  dropdownIcon,
+  options,
 }) => (
   <Select.Root>
     <Select.Trigger
       className="inline-flex h-[35px] w-full items-center justify-between rounded-lg border border-slate-600 bg-white px-2 font-mont text-[13px] leading-none text-slate-600 outline-none hover:bg-slate-200 data-[placeholder]:text-black/40"
       aria-label="Food"
     >
-      <div className="flex items-center gap-1">
-        <span className="[&_svg]:h-[25px] [&_svg]:w-[25px]">
-          <FilterIcon />
-        </span>
+      <div className="flex items-center">
+        {dropdownIcon}
         <Select.Value placeholder={placeholder} />
       </div>
       <Select.Icon className="text-slate-600">
-        <div className="rotate-180">
+        <div className="ml-2 rotate-180">
           <ChevronIcon />
         </div>
       </Select.Icon>
@@ -35,48 +41,21 @@ const SelectDropdown: React.FC<Props> = ({
       >
         <Select.Viewport className="p-2">
           <Select.Group>
-            {/* <Select.Label className="p-1 text-xs leading-[25px]">
-              Fruits
-            </Select.Label> */}
-            <SelectItem
-              className="cursor-pointer rounded-lg border-none p-2 text-sm outline-none hover:bg-slate-200"
-              value="all"
-              icon={
-                <div className="mr-2 inline-flex h-[8px] w-[8px] rounded-full border border-slate-400" />
-              }
-            >
-              All
-            </SelectItem>
-            <SelectItem
-              className="cursor-pointer rounded-lg border-none p-2 text-sm outline-none hover:bg-slate-200"
-              value="online"
-              icon={
-                <div className="mr-2 inline-flex h-[8px] w-[8px] rounded-full bg-online" />
-              }
-            >
-              Online
-            </SelectItem>
-            <SelectItem
-              className="cursor-pointer rounded-lg border-none p-2 text-sm outline-none hover:bg-slate-200"
-              value="offline"
-              icon={
-                <div className="mr-2 inline-flex h-[8px] w-[8px] rounded-full bg-offline" />
-              }
-            >
-              Offline
-            </SelectItem>
-            <SelectItem
-              className="cursor-pointer rounded-lg border-none p-2 text-sm outline-none hover:bg-slate-200"
-              value="spotty"
-              icon={
-                <div className="mr-2 inline-flex h-[8px] w-[8px] rounded-full bg-spotty" />
-              }
-            >
-              Spotty
-            </SelectItem>
-          </Select.Group>
+            {options?.map((item, i) => {
+              const key = `${item.value}-${i}`;
 
-          <Select.Separator className="bg-violet6 m-[5px] h-[1px]" />
+              return (
+                <SelectItem
+                  className="cursor-pointer rounded-lg border-none p-2 text-sm outline-none hover:bg-slate-200"
+                  key={key}
+                  value={item.value}
+                  icon={item.icon}
+                >
+                  {item.text}
+                </SelectItem>
+              );
+            })}
+          </Select.Group>
         </Select.Viewport>
         <Select.ScrollDownButton className="flex h-[25px] cursor-default items-center justify-center bg-white text-slate-600">
           <div className="rotate-180">
