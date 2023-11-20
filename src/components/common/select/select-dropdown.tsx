12e7bf3e128114cb5758d41fdc697374fbc6cf19
waleedbinalim <waleedbinalim@gmail.com>
2023-11-20 16:32:13 +0500

    chore: cleanup on dropdown component

--- src/components/common/select/select-dropdown.tsx
+++ src/components/common/select/select-dropdown.tsx
@@ -1,29 +1,34 @@
+import { ChevronIcon } from "@/assets";
 import * as Select from "@radix-ui/react-select";
 import React, { ReactNode } from "react";
 
-const SelectDropdown: React.FC = () => (
+type Props = {
+  placeholder?: string;
+};
+
+const SelectDropdown: React.FC<Props> = ({
+  placeholder = "placeholder...",
+}) => (
   <Select.Root>
     <Select.Trigger
-      className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-slate-600 shadow-[0_2px_10px] shadow-black/10 hover:bg-blue-400 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet-bg-violet-900 outline-none w-full"
+      className="inline-flex h-[35px] w-full items-center justify-between gap-[5px] rounded bg-white px-[15px] text-[13px] leading-none text-slate-600 shadow-[0_2px_10px] shadow-black/10 outline-none hover:bg-blue-400 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-black/40"
       aria-label="Food"
     >
-      <Select.Value placeholder="Select a fruit…" />
+      <Select.Value placeholder={placeholder} />
       <Select.Icon className="text-slate-600">
-        {/* CHEVRON DOWN ICON HERE */}
-        {/* <ChevronDownIcon /> */}
-        <p className="rotate-180">^</p>
+        <div className="rotate-180">
+          <ChevronIcon />
+        </div>
       </Select.Icon>
     </Select.Trigger>
     <Select.Portal>
-      <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-        <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-slate-600 cursor-default">
-          {/* CHEVRON UP ICON HERE */}
-          {/* <ChevronUpIcon /> */}
-          <p>^</p>
+      <Select.Content className="overflow-hidden rounded-md bg-white shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+        <Select.ScrollUpButton className="flex h-[25px] cursor-default items-center justify-center bg-white text-slate-600">
+          <ChevronIcon />
         </Select.ScrollUpButton>
         <Select.Viewport className="p-[5px]">
           <Select.Group>
-            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+            <Select.Label className="text-mauve11 px-[25px] text-xs leading-[25px]">
               Fruits
             </Select.Label>
             <SelectItem value="apple">Apple</SelectItem>
@@ -33,10 +38,10 @@ const SelectDropdown: React.FC = () => (
             <SelectItem value="pineapple">Pineapple</SelectItem>
           </Select.Group>
 
-          <Select.Separator className="h-[1px] bg-violet6 m-[5px]" />
+          <Select.Separator className="bg-violet6 m-[5px] h-[1px]" />
 
           <Select.Group>
-            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+            <Select.Label className="text-mauve11 px-[25px] text-xs leading-[25px]">
               Vegetables
             </Select.Label>
             <SelectItem value="aubergine">Aubergine</SelectItem>
@@ -48,10 +53,10 @@ const SelectDropdown: React.FC = () => (
             <SelectItem value="leek">Leek</SelectItem>
           </Select.Group>
 
-          <Select.Separator className="h-[1px] bg-violet6 m-[5px]" />
+          <Select.Separator className="bg-violet6 m-[5px] h-[1px]" />
 
           <Select.Group>
-            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+            <Select.Label className="text-mauve11 px-[25px] text-xs leading-[25px]">
               Meat
             </Select.Label>
             <SelectItem value="beef">Beef</SelectItem>
@@ -60,10 +65,10 @@ const SelectDropdown: React.FC = () => (
             <SelectItem value="pork">Pork</SelectItem>
           </Select.Group>
         </Select.Viewport>
-        <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-slate-600 cursor-default">
-          {/* CHEVRON DOWN ICON HERE */}
-          {/* <ChevronDownIcon /> */}
-          <p className="rotate-180">^</p>
+        <Select.ScrollDownButton className="flex h-[25px] cursor-default items-center justify-center bg-white text-slate-600">
+          <div className="rotate-180">
+            <ChevronIcon />
+          </div>
         </Select.ScrollDownButton>
       </Select.Content>
     </Select.Portal>
@@ -83,26 +88,26 @@ const SelectItem = React.forwardRef(
       className?: string | undefined;
       disabled?: boolean;
     },
-    forwardedRef?: React.ForwardedRef<HTMLDivElement>
+    forwardedRef?: React.ForwardedRef<HTMLDivElement>,
   ) => {
     return (
       <Select.Item
         className={
           className ??
-          "text-[13px] leading-none text-slate-600 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet-900 data-[highlighted]:text-white"
+          "data-[disabled]:text-mauve8 relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none text-slate-600 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-900 data-[highlighted]:text-white data-[highlighted]:outline-none"
         }
         {...props}
         value={value}
         ref={forwardedRef}
       >
         <Select.ItemText>{children}</Select.ItemText>
-        <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+        <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
           {/* CHECK ICON HERE */}
           {/* <CheckIcon /> */}
         </Select.ItemIndicator>
       </Select.Item>
     );
-  }
+  },
 );
 
 SelectItem.displayName = "SelectItem";
