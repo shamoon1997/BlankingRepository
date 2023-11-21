1d85552d9f65881e57b07ba9a753912c1f136715
shamoon1997 <sshamoon83@gmail.com>
2023-11-21 17:51:19 +0500

    style: tailwind classes changed

--- src/components/map/map-tooltip/map-tooltip.tsx
+++ src/components/map/map-tooltip/map-tooltip.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const MapToolTipContainer = ({ children }: Props) => {
   return (
-    <div className="w-full max-w-[188px] rounded border border-solid border-[rgba(91,91,91,0.50)] bg-white/90 shadow-tooltip">
+    <div className="absolute rounded border-[0.5px] border-solid border-[rgba(91,91,91,0.50)] bg-white/90 shadow-tooltip">
       {children}
     </div>
   );
