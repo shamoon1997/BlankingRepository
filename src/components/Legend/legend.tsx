8e09d94bfc74f16772ca1cde56290e35882961ed
shamoon1997 <sshamoon83@gmail.com>
2023-11-21 17:17:03 +0500

    UI: legend ui completed

--- src/components/Legend/legend.tsx
+++ src/components/Legend/legend.tsx
@@ -1,43 +1,41 @@
 function Legend() {
   return (
-    <div className="max-w-150 max-h-117 absolute bottom-12 right-10">
-      <div className="h-117 w-full flex-shrink-0 rounded-md border border-solid border-gray-500 border-opacity-50 bg-white shadow-md">
-        <div className="flex flex-col gap-1.5 p-4">
-          <div className="flex items-center justify-evenly">
-            <div className="h-2 w-2 rounded-full bg-gray-700" />
+    <div className="absolute bottom-12 right-10">
+      <div className="border-[rgba(91, 91, 91, 0.50)] flex w-full max-w-[150px] flex-col gap-2.5 rounded border border-solid border-opacity-50 bg-white px-3 py-3.5 shadow-tooltip">
+        <div className="flex items-center gap-[9px]">
+          <div className="h-2 w-2 shrink-0 rounded-full bg-gray-700" />
 
-            <div className="font-mont text-sm font-normal leading-normal tracking-tighter text-gray-900">
-              Undetermined
-            </div>
+          <div className="font-mont text-[10px] font-normal leading-[normal] tracking-[-0.5px] text-[#161616]">
+            Undetermined
           </div>
+        </div>
 
-          <div className="flex items-center justify-evenly">
-            <div className="h-2 w-2 rounded-full bg-purple-700" />
+        <div className="flex items-center gap-[9px]">
+          <div className="h-2 w-2 shrink-0 rounded-full bg-purple-700" />
 
-            <div className="font-mont text-sm font-normal leading-normal tracking-tighter text-gray-900">
-              To be investigated
-            </div>
+          <div className="font-mont text-[10px] font-normal leading-[normal] tracking-[-0.5px] text-[#161616]">
+            To be investigated
           </div>
+        </div>
 
-          <div className="flex items-center justify-evenly">
-            <div className="h-2 w-2 rounded-full bg-red-600" />
-            <div className="font-mont text-sm font-normal leading-normal tracking-tighter text-gray-900">
-              To-do
-            </div>
+        <div className="flex items-center gap-[9px]">
+          <div className="h-2 w-2 shrink-0 rounded-full bg-red-600" />
+          <div className="font-mont text-[10px] font-normal leading-[normal] tracking-[-0.5px] text-[#161616]">
+            To-do
           </div>
+        </div>
 
-          <div className="flex items-center justify-evenly">
-            <div className="h-2 w-2 rounded-full bg-yellow-400" />
-            <div className="font-mont text-sm font-normal leading-normal tracking-tighter text-gray-900">
-              In-progress
-            </div>
+        <div className="flex items-center gap-[9px]">
+          <div className="h-2 w-2 shrink-0 rounded-full bg-yellow-400" />
+          <div className="font-mont text-[10px] font-normal leading-[normal] tracking-[-0.5px] text-[#161616]">
+            In-progress
           </div>
+        </div>
 
-          <div className="flex justify-center">
-            <div className="h-2 w-2 rounded-full bg-blue-500" />
-            <div className="font-mont text-sm font-normal leading-normal tracking-tighter text-gray-900">
-              Reported To Customer
-            </div>
+        <div className="flex items-center gap-[9px]">
+          <div className="h-2 w-2 shrink-0 rounded-full bg-blue-500" />
+          <div className="font-mont text-[10px] font-normal leading-[normal] tracking-[-0.5px] text-[#161616]">
+            Reported To Customer
           </div>
         </div>
       </div>
