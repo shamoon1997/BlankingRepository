ae8862a48efa9f9b49ba20054884e4c71df18694
waleedbinalim <waleedbinalim@gmail.com>
2023-11-23 16:10:17 +0500

    chore: added base header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { NotificationIcon, ProfileIcon } from "@/assets";
 import React from "react";
 
 interface HeaderProps {
@@ -6,8 +7,18 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ heading }) => {
   return (
-    <div className="text-24 leading-31 tracking-0 text-left font-mont font-bold">
-      {heading}
+    <div className="flex min-h-[40px] w-full justify-between bg-slate-100 px-2 py-4">
+      <div className="grid place-content-center">
+        <h1 className="text-2xl">{heading}</h1>
+      </div>
+      <div className="flex gap-2">
+        <div className="grid h-[28px] w-[28px] place-content-center rounded-full bg-slate-200 [&_svg]:h-[12px] [&_svg]:w-[12px]">
+          <ProfileIcon />
+        </div>
+        <div className="grid h-[28px] w-[28px] place-content-center rounded-full bg-slate-200 [&_svg]:h-[12px] [&_svg]:w-[12px]">
+          <NotificationIcon />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/deployments-page.tsx
+++ src/pages/deployments-page.tsx
@@ -1,16 +1,22 @@
 import { BaseMap, PageLoader } from "@/components";
+import Header from "@/components/Header";
 import { FilterArea } from "@/components/filters";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 
 export const DeploymentPage = withAuthenticationRequired(
   () => {
     return (
-      <div className="flex flex-1">
-        <div>
-          <FilterArea />
-        </div>
-        <div className="relative w-full">
-          <BaseMap />
+      <div className="flex w-full flex-1 flex-col">
+        {/* NAVBAR */}
+        <Header heading="Deployment" />
+
+        <div className="flex flex-1">
+          <div>
+            <FilterArea />
+          </div>
+          <div className="relative w-full">
+            <BaseMap />
+          </div>
         </div>
       </div>
     );
