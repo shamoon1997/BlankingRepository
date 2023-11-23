2481ceaec15246f6d4337dfb6b601facfe750a34
waleedbinalim <waleedbinalim@gmail.com>
2023-11-23 16:43:11 +0500

    fix: bell icon and dropdown placed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { NotificationIcon, ProfileIcon } from "@/assets";
 import React from "react";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 interface HeaderProps {
   heading: string;
@@ -13,11 +14,29 @@ const Header: React.FC<HeaderProps> = ({ heading }) => {
       </div>
       <div className="flex gap-2">
         <div className="grid h-[28px] w-[28px] place-content-center rounded-full bg-slate-200 [&_svg]:h-[12px] [&_svg]:w-[12px]">
-          <ProfileIcon />
-        </div>
-        <div className="grid h-[28px] w-[28px] place-content-center rounded-full bg-slate-200 [&_svg]:h-[12px] [&_svg]:w-[12px]">
           <NotificationIcon />
         </div>
+
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <div className="grid h-[28px] w-[28px] place-content-center rounded-full bg-slate-200 [&_svg]:h-[12px] [&_svg]:w-[12px]">
+              <ProfileIcon />
+            </div>
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="z-[3] rounded-md border border-blue-400 bg-white p-[5px]"
+              sideOffset={5}
+            >
+              <DropdownMenu.Item className="relative flex h-[25px] cursor-pointer items-center rounded-[3px] px-4 text-[13px] outline-none hover:bg-slate-200">
+                Sign out
+              </DropdownMenu.Item>
+
+              <DropdownMenu.Arrow className="fill-white" />
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
       </div>
     </div>
   );
