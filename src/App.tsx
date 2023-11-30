5d10d46aebbb47e32878a2ed439e5444b7327676
waleedbinalim <waleedbinalim@gmail.com>
2023-11-30 17:02:05 +0500

    chore: removed test page to preview the ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import { CallbackPage, DeploymentPage, HomePage, NotFoundPage } from "./pages";
 import { useAuth0 } from "@auth0/auth0-react";
-import {
-  HomeLoader,
-  PageLoader,
-  PoleViewTabGroup,
-  SideNavigation,
-} from "./components";
+import { HomeLoader, PageLoader, SideNavigation } from "./components";
 import { AppRoutes } from "./utils/routes";
 import { useShowSideBar } from "./hooks";
 
@@ -26,10 +21,6 @@ const RouteMappings = [
   {
     path: AppRoutes.notFound,
     element: <NotFoundPage />,
-  },
-  {
-    path: "/test",
-    element: <PoleViewTabGroup />,
   },
 ];
 
