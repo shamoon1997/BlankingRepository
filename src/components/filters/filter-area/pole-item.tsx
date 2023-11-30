1dd657858b7393bd78d262b6c7eb0107f42a19fc
waleedbinalim <waleedbinalim@gmail.com>
2023-11-30 16:56:48 +0500

    chore: bg change for selected devices in sidebar

--- src/components/filters/filter-area/pole-item.tsx
+++ src/components/filters/filter-area/pole-item.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { PoleViewButton } from "./pole-view-button";
 
 export const PoleItem = () => {
+  const [selected, setSelected] = useState<boolean>(false);
+
   return (
-    <div className="ml-4 mr-4 flex h-10 items-center justify-between">
-      <p className="text-primary text-xs font-semibold">1535142 • GS1245</p>
+    <div
+      className={`flex h-10 cursor-pointer items-center justify-between pl-4 pr-4 ${
+        selected ? "border-[0.3px] border-y-[#DFDFDF] bg-[#F2F2F2]" : ""
+      }`}
+      onClick={() => setSelected(!selected)}
+    >
+      <p className="text-xs font-semibold text-primary">1535142 • GS1245</p>
       <PoleViewButton />
     </div>
   );
