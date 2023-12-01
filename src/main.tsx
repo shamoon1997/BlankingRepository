5c10550414f496960f9fe7255936266a40ade71a
shamoon1997 <sshamoon83@gmail.com>
2023-12-01 18:01:42 +0500

    feat: react boundary added

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./fonts.css";
 import "./index.css";
+import { FallBackPage } from "./pages/fall-back-page.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 import { Auth0ProviderWithNavigate } from "./state/providers";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={FallBackPage}>
+      <BrowserRouter>
+        <Auth0ProviderWithNavigate>
+          <App />
+        </Auth0ProviderWithNavigate>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
