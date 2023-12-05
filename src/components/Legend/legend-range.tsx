0e61d54aaecbfed284f13d941ca3434a420a41c2
shamoon1997 <sshamoon83@gmail.com>
2023-12-05 10:50:02 +0500

    chore: Pr updated based on the review

--- src/components/Legend/legend-range.tsx
+++ src/components/Legend/legend-range.tsx
@@ -1,25 +1,32 @@
 function LegendRange() {
+  const colorArray = [
+    "#5C88FF",
+    "#5546FF",
+    "#A974FF",
+    "#D92AF6",
+    "#FF238D",
+    "#F00",
+  ];
+
+  const rangeArray = [8, 283, 567];
+
   return (
     <div className="absolute bottom-12 right-10">
       <div className="border-[rgba(91, 91, 91, 0.50)] flex max-w-[270px] flex-shrink-0 flex-col rounded-[4px] border-[0.5px] border-solid bg-white p-3 shadow-tooltip">
         <div className="flex overflow-hidden rounded-[10px]">
-          <div className="h-[5px] w-[40px] bg-[#5C88FF]" />
-          <div className="h-[5px] w-[40px] bg-[#5546FF]" />
-          <div className="h-[5px] w-[40px] bg-[#A974FF]" />
-          <div className="h-[5px] w-[40px] bg-[#D92AF6]" />
-          <div className="h-[5px] w-[40px] bg-[#FF238D]" />
-          <div className="h-[5px] w-[40px] bg-[#F00]" />
+          {colorArray.map((color, index) => (
+            <div className={`h-[5px] w-[40px] bg-[${color}]`} key={index} />
+          ))}
         </div>
         <div className="mt-1 flex justify-between">
-          <div className="font-mont text-[10px] font-normal leading-normal tracking-tight text-[#161616]">
-            8
-          </div>
-          <div className="font-mont text-[10px] font-normal leading-normal tracking-tight text-[#161616]">
-            283
-          </div>
-          <div className="font-mont text-[10px] font-normal leading-normal tracking-tight text-[#161616]">
-            567
-          </div>
+          {rangeArray.map((value, index) => (
+            <div
+              className="font-mont text-[10px] font-normal leading-normal tracking-tight text-[#161616]"
+              key={index}
+            >
+              {value}
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,9 +3,9 @@ import { BaseMap } from "./map";
 import { MapToolTipContainer } from "./map/map-tooltip";
 import { PageLoader } from "./page-loaders/page-loader";
 import { HomeLoader } from "./page-loaders/home-loader";
-import LegendTodo from "./Legend/legend-todo";
-import LegendStatus from "./Legend/legend-status";
-import LegendRange from "./Legend/legend-range";
+import LegendTodo from "./legend/legend-todo";
+import LegendStatus from "./legend/legend-status";
+import LegendRange from "./legend/legend-range";
 import MapHoverMenu from "./map/map-hover-menu/map-hover-menu";
 import SideNavigation from "./side-navigation";
 
