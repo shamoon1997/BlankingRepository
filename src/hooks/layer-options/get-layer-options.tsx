96b9431e715a9653757e7ccd398eb47ea1e42a81
shamoon1997 <sshamoon83@gmail.com>
2023-12-11 13:40:31 +0500

    fix: search query filtering corrected

--- src/hooks/layer-options/get-layer-options.tsx
+++ src/hooks/layer-options/get-layer-options.tsx
@@ -1,14 +1,28 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { z, ZodError } from "zod";
 
-export const useLayerOptions = (): string | null => {
-  const [gridscopeOptions, setGridscopeOptions] = useState<string | null>(null);
+const LayerOptions = z.enum(["equipment", "heatmap"]);
+export type LayerOptionsType = z.infer<typeof LayerOptions>;
+
+export const useLayerOptions = (): LayerOptionsType | null => {
+  const [gridscopeOptions, setGridscopeOptions] =
+    useState<LayerOptionsType | null>(null);
   const location = useLocation();
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const optionsParam = searchParams.get("layer");
-    setGridscopeOptions(optionsParam);
+
+    try {
+      const parsedOptions = LayerOptions.parse(optionsParam);
+      setGridscopeOptions(parsedOptions);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        console.error("Invalid layer option:", error.errors);
+        setGridscopeOptions(null);
+      }
+    }
   }, [location]);
 
   return gridscopeOptions;
