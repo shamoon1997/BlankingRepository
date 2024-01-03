6c96d1d9bf8e6e6de4a10c399a008041a67762bb
waleedbinalim <waleedbinalim@gmail.com>
2024-01-03 18:03:52 +0500

    chore: more ui work

--- src/components/metrics/device-data/metrics-data-tab.tsx
+++ src/components/metrics/device-data/metrics-data-tab.tsx
@@ -91,21 +91,30 @@ const MetricsDataTab: React.FC = () => {
           className="bg-[#F5F5F5] px-[12px] py-[10px] text-[12px]"
           value="item-1"
         >
-          <Accordion.Trigger className="group">
+          <Accordion.Trigger className="group w-full">
             <div className="flex items-center gap-[20px]">
               <div className="h-[12px] w-[12px] group-data-[state=closed]:rotate-180 group-data-[state=open]:rotate-0">
                 <ChevronIcon />
               </div>
-              Is it accessible?
+              <p>
+                Normalized Electrometer Median Goertzel Delta{" "}
+                <span className="ml-[15px] text-[#8A8A8A]">(1 panel)</span>
+              </p>
             </div>
           </Accordion.Trigger>
           <Accordion.Content>
             <div className="mt-[12px] h-[0.5px] w-full bg-[#D9D9D9]" />
 
-            <div className="flex justify-between">
-              <img src="/images/mock-metric-data-img.png" alt="" />
+            <div className="flex justify-between gap-[10px] pb-[10px]">
+              <div className=" w-3/4">
+                <img
+                  className="max-h-[320px] w-full bg-cover"
+                  src="/images/mock-metric-data-img.png"
+                  alt=""
+                />
+              </div>
 
-              <div className="flex w-[207px] flex-col">
+              <div className="mt-[9px] flex w-1/4 flex-col">
                 <h3 className="mb-[8px]">Graph View</h3>
 
                 <div className="mb-[8px] flex items-center gap-[5px]">
@@ -115,7 +124,7 @@ const MetricsDataTab: React.FC = () => {
                     placeholder="Search to add device"
                   />
                   <button className="grid h-[25px] w-[25px] place-content-center rounded-md bg-[#3B3C4F]">
-                    <div className="h-[12px] w-[12px] [&_path]:fill-white [&circle]:stroke-white ">
+                    <div className="h-[12px] w-[12px] [&_circle]:stroke-white [&_path]:fill-white">
                       <SearchIcon />
                     </div>
                   </button>
@@ -125,23 +134,32 @@ const MetricsDataTab: React.FC = () => {
                   Deployment: Birmingham (10)
                 </h3>
 
-                <div>
+                <div className="no-scrollbar max-h-[250px] overflow-y-scroll">
                   {/* SELECTED HERE */}
 
-                  <div className="mb-[8px] flex items-center gap-[5px]">
-                    <div className="flex h-[25px] w-[180px] items-center overflow-hidden rounded-md border-[0.8px] border-[#CCCCCC] bg-white text-[8px]">
-                      <div className="grid h-[25px] w-[25px] place-content-center bg-blue-400 [&_path]:fill-white">
-                        <LocationIcon />
-                      </div>
+                  {[...Array(16)]?.map((_, i) => {
+                    return (
+                      <div
+                        className="mb-[8px] flex items-center gap-[5px]"
+                        key={`mock-metrics-${i}`}
+                      >
+                        <div className="flex h-[25px] w-[180px] items-center overflow-hidden rounded-md border-[0.8px] border-[#CCCCCC] bg-white text-[8px]">
+                          <div className="grid h-[25px] w-[25px] place-content-center bg-[#B7B7B7] [&_path]:fill-white">
+                            <LocationIcon />
+                          </div>
 
-                      <p className="ml-[6px] font-medium">GS124• 4024 • Lora</p>
-                    </div>
-                    <button className="grid h-[25px] w-[25px] place-content-center rounded-md border-[0.8px] border-[#CCCCCC] bg-white">
-                      <div className="grid h-[12px] w-[12px] rotate-45 place-content-center ">
-                        <CrossIcon />
+                          <p className="ml-[6px] font-medium">
+                            GS124• 4024 • Lora
+                          </p>
+                        </div>
+                        <button className="grid h-[25px] w-[25px] place-content-center rounded-md border-[0.8px] border-[#CCCCCC] bg-white">
+                          <div className="grid h-[12px] w-[12px] rotate-45 place-content-center ">
+                            <CrossIcon />
+                          </div>
+                        </button>
                       </div>
-                    </button>
-                  </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
@@ -152,7 +170,7 @@ const MetricsDataTab: React.FC = () => {
           className="bg-[#F5F5F5] px-[12px] py-[10px] text-[12px]"
           value="item-2"
         >
-          <Accordion.Trigger className="group">
+          <Accordion.Trigger className="group w-full">
             <div className=" flex items-center gap-[20px]">
               <div className="h-[12px] w-[12px] group-data-[state=closed]:rotate-180 group-data-[state=open]:rotate-0">
                 <ChevronIcon />
@@ -170,7 +188,7 @@ const MetricsDataTab: React.FC = () => {
           className="bg-[#F5F5F5] px-[12px] py-[10px] text-[12px]"
           value="item-3"
         >
-          <Accordion.Trigger className="group">
+          <Accordion.Trigger className="group w-full">
             <div className="flex items-center gap-[20px]">
               <div className="h-[12px] w-[12px] group-data-[state=closed]:rotate-180 group-data-[state=open]:rotate-0">
                 <ChevronIcon />
