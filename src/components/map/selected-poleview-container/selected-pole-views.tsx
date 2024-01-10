773bb891faf9d76d53006a9077afb87cb0c67742
shamoon1997 <sshamoon83@gmail.com>
2024-01-10 14:16:51 +0500

    fix: snippet minimize fixed

--- src/components/map/selected-poleview-container/selected-pole-views.tsx
+++ src/components/map/selected-poleview-container/selected-pole-views.tsx
@@ -11,18 +11,13 @@ export const SelectedPoleViews = ({ selectedPoles }: SelectedPoleViewProps) => {
         selectedPoles.length > 0 ? "pl-3 pr-3" : ""
       }  absolute bottom-4 top-16 z-[15] flex gap-3`}
     >
-      {selectedPoles
-        .slice()
-        .sort((a, b) =>
-          a.isMinimized === b.isMinimized ? 0 : a.isMinimized ? 1 : -1,
-        )
-        .map((selectedPole) => (
-          <MapPopup
-            selectedPoleHardwareId={selectedPole.selectedPoleHardwareId}
-            isMinimized={selectedPole.isMinimized}
-            key={selectedPole.selectedPoleHardwareId}
-          />
-        ))}
+      {selectedPoles.map((selectedPole) => (
+        <MapPopup
+          selectedPoleHardwareId={selectedPole.selectedPoleHardwareId}
+          isMinimized={selectedPole.isMinimized}
+          key={selectedPole.selectedPoleHardwareId}
+        />
+      ))}
     </div>
   );
 };
