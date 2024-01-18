5d8bd901e3898423c7871f9ae1646321b9c00ca5
waleedbinalim <waleedbinalim@gmail.com>
2024-01-18 17:52:47 +0500

    chore: got the octet stream working

--- src/api/device-data/metric-data.api.ts
+++ src/api/device-data/metric-data.api.ts
@@ -1,21 +1,6 @@
-import { AxiosResponse } from "axios";
 import { API, ApiResources } from "../config/api-config";
 import { MetricDataresponseType } from "../types/types";
-
-// CURRENTLY HARDCODED WITH IDS
-
-export const getMetricDataAPI = (): Promise<
-  AxiosResponse<MetricDataresponseType[]>
-> => {
-  return API.post(ApiResources.getHardwareMetricData, {
-    hardware_ids: [
-      "3f0017000c5030415738382000000001",
-      "0f003c000e5030475837322000000001",
-    ],
-    t1: "2023-11-24 21:00:00",
-    t2: "2023-11-24 21:30:00",
-  });
-};
+import { decompressSync, strFromU8 } from "fflate";
 
 type HardwareMetricDataPayload = {
   hardware_ids: string[];
@@ -29,22 +14,40 @@ type DeploymentMetricDataPayload = {
   t2: string;
 };
 
-export const getHardwareMetricDataAPI = ({
+export const getHardwareMetricDataAPI = async ({
   hardware_ids,
   t1,
   t2,
-}: HardwareMetricDataPayload): Promise<
-  AxiosResponse<MetricDataresponseType[]>
-> => {
-  return API.post(ApiResources.getHardwareMetricData, { hardware_ids, t1, t2 });
+}: HardwareMetricDataPayload): Promise<MetricDataresponseType[]> => {
+  return API.post(ApiResources.getHardwareMetricData, { hardware_ids, t1, t2 })
+    .then((response) => {
+      const res = decompressSync(new Uint8Array(response.data));
+      const origText = strFromU8(res);
+      const jsonResponse: MetricDataresponseType[] = JSON.parse(origText);
+      return jsonResponse;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
-export const getDeploymentMetricDataAPI = ({
+export const getDeploymentMetricDataAPI = async ({
   deployment,
   t1,
   t2,
-}: DeploymentMetricDataPayload): Promise<
-  AxiosResponse<MetricDataresponseType[]>
-> => {
-  return API.post(ApiResources.getDeploymentMetricData, { deployment, t1, t2 });
+}: DeploymentMetricDataPayload): Promise<MetricDataresponseType[]> => {
+  return API.post(
+    ApiResources.getDeploymentMetricData,
+    { deployment, t1, t2 },
+    { responseType: "arraybuffer" },
+  )
+    .then((response) => {
+      const res = decompressSync(new Uint8Array(response.data));
+      const origText = strFromU8(res);
+      const jsonResponse: MetricDataresponseType[] = JSON.parse(origText);
+      return jsonResponse;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
